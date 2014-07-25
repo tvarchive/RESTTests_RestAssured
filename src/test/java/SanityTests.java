964798295be1672de97c800a517e21e966a95472
Pani <kumarpani@gmail.com>
2014-07-25 16:14:52 +0530

    Chapter2: BDD style test

--- src/test/java/SanityTests.java
+++ src/test/java/SanityTests.java
@@ -1,14 +1,33 @@
-import com.jayway.restassured.RestAssured;
 import org.junit.Test;
+
+import static com.jayway.restassured.RestAssured.get;
+import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 public class SanityTests {
 
+    // Simple JSON Validation
     @Test
     public void shouldReturnExactlyOneItem() {
-        RestAssured.get("http://gdata.youtube.com/feeds/api/videos?q=sAPUfRycSes&v=2&alt=jsonc")
-                .then()
-                .assertThat()
+        get("http://gdata.youtube.com/feeds/api/videos?q=sAPUfRycSes&v=2&alt=jsonc")
+            .then()
+            .assertThat()
+            .body("data.totalItems", equalTo(1));
+    }
+
+    // Using parameters and BDD style for better readability
+    @Test
+    public void shouldAlsoReturnOneItem() {
+        given()
+                .param("q", "sAPUfRycSes")
+                .param("v", "2")
+                .param("alt", "jsonc").
+        when()
+                .get("http://gdata.youtube.com/feeds/api/videos").
+        then()
                 .body("data.totalItems", equalTo(1));
     }
+
+
+
 }
